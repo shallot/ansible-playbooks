6353348a225e0d7ea3d6e1ae5fb0d32b2e9b7421
Josip Rodin <j.rodin@eyeo.com>
2021-07-13 13:53:50 +0200

    Implement systemd unit types and raw content passing
    systemd supports both service and socket definitions, so add a type
    attribute to supports socket unit files
    
    As socket and service unit files can be named the same, also support
    a name attribute to distinguish them
    
    Supporting extra attributes required passing more of this metadata through
    ini_file_from_dict
    
    Support passing in a string content of the unit file instead of
    our dictionaries, for easier importing of pre-existing unit files

--- filter_plugins/ini_input_from_dict.py
+++ filter_plugins/ini_input_from_dict.py
@@ -1,3 +1,6 @@
+#import sys
+#from pprint import pformat
+
 class FilterModule(object):
     def filters(self):
         return { 'ini_input_from_dict': self.ini_input_from_dict }
@@ -6,15 +9,19 @@ class FilterModule(object):
         ret = []
         for ini_file, configurations in arg.items():
             if type(configurations) is not dict:
+#                sys.stderr.write("configurations: " + str(configurations) + "\n")
                 continue
+            config = {}
             for configuration, sections in configurations.items():
                 if type(sections) is not dict:
+                    config[configuration] = sections
                     continue
+#                sys.stderr.write("found dict sections, will now use config: " + pformat(config) + "\n")
                 for section, optionvaluepairs in sections.items():
                     for option, value in optionvaluepairs.items():
                         ret.append(dict(
                                     ini_file=ini_file,
-                                    configuration=configuration,
+                                    config=config,
                                     section=section,
                                     option=option,
                                     value=value
