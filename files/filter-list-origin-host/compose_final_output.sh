cb8e9611dd52dfb629b652cce998124e83d2def4
Manuel Niedermeier <m.niedermeier@eyeo.com>
2022-11-16 16:09:03 +0100

    Retain multiple versions of final_output in filter-list-origin setup
    For potential debugging reasons, we want to retain multiple versions
    of what got generated as 'final_output'.
    
    This also utilizes a retention policy, that can be adjusted.
    
    Usage of logrotate to have multiple conditions for when the files
    are rotated, e.g. file age & number of files.
    
    Set lower value for retention versions on example machine.
    Otherwise the persistent CI VM can't handle the used disk space.

--- files/filter-list-origin-host/compose_final_output.sh
+++ files/filter-list-origin-host/compose_final_output.sh
@@ -2,6 +2,7 @@
 
 prepdir={{ filter_list_origin_final_output }}_prep
 finaldir={{ filter_list_origin_final_output }}
+olddir={{ filter_list_origin_final_output }}_old
 
 link_globs=$@
 
@@ -17,10 +18,19 @@ cp -a {{ filter_list_origin_outputs }}/gstorage_artifacts/. $prepdir/
 # this replaces regular files with links to subdir files, if they exist
 {{ filter_list_origin_base_dir }}/compound_webroot_structure.sh $prepdir "$link_globs"
 
-rm -rf $finaldir.old
-test ! -d $finaldir || mv $finaldir $finaldir.old
+# clean up olddir, if it exists
+rm -rf $olddir
+
+test ! -d $finaldir || mv $finaldir $olddir
 # minimal race condition is here - this is where we don't want anyone reading from $finaldir
 # but rsync --max-delete=anything on filter-servers protects us from downtime here
 mv $prepdir $finaldir
 
 # $finaldir is referenced from ssh forced command
+
+test -d $olddir && tar --remove-files -vcf \
+   {{ filter_list_origin_archive }}/final_output_old.tar $olddir
+
+# Delete old versions of final_output
+/usr/sbin/logrotate -f -v -s {{ filter_list_origin_archive }}/status \
+    {{ filter_list_origin_base_dir }}/rotate-final-output
